1003acea4f06b56e6a83d85c3aee3dc4846f53f5
Augusto Emiliano Portillo <porti.sk8@gmail.com>
2022-07-04 20:56:54 -0300

    First commit

---
+++ ByB.Comedor.DB/dbo/Tables/TEST.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[TEST] (
+    [TestId]      INT           IDENTITY (1, 1) NOT NULL,
+    [Descripcion] VARCHAR (MAX) NULL
+);
+
---
+++ ByB.Comedor.DB/usuario/Tables/Usuario.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [usuario].[Usuario] (
+    [UsuarioId] INT           IDENTITY (1, 1) NOT NULL,
+    [Username]  VARCHAR (250) NULL,
+    [Email]     VARCHAR (250) NULL,
+    [Password]  VARCHAR (MAX) NULL,
+    [Salt]      VARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([UsuarioId] ASC)
+);
+
