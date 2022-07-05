d152a8d0ca2e021ed473e7d17530497f8dabe7e1
Augusto Emiliano Portillo <porti.sk8@gmail.com>
2022-07-05 00:01:17 -0300

    ByB Comedor DB

---
+++ ByB.Comedor.DB/dbo/Tables/Comedores.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Comedores] (
+    [ComedorId]   INT           NOT NULL,
+    [Descripcion] VARCHAR (250) NOT NULL,
+    [Titulo]      VARCHAR (250) NOT NULL,
+    CONSTRAINT [PK_Comedores] PRIMARY KEY CLUSTERED ([ComedorId] ASC)
+);
+
